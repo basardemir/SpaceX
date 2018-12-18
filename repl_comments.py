a8953f56e2d15001b34588a6076701a1ab3a3563
demirba18 <43862644+demirba18@users.noreply.github.com>
2018-12-18 17:12:03 +0300

    Update repl_comments.py
    I add password system

--- repl_comments.py
+++ repl_comments.py
@@ -1,9 +1,21 @@
-cmt_list = []
+from hashlib import sha256
+
+cmnt_list = []
+
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+
 while True:
-    comment = input("Enter your comment: ")
-    cmt_list.append(comment)
-    print("Previously entered comments: ")
-    j = 0
-    while len(cmt_list) >= j+1:
-        print(str(j+1) + ". " + cmt_list[j])
-        j = j+1
+    pswrd = input('Enter your password: ')
+    hsh1 = create_hash(pswrd)
+    if hsh1 == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92":
+        comment = input("Enter your comment: ")
+        cmnt_list.append(comment)
+        print("Previously entered comments: ")
+        j = 0
+        while len(cmnt_list) >= j+1:
+            print(str(j+1) + ". " + cmnt_list[j])
+            j = j+1
+    else:
+        print("I am sorry I can't let you do that.")
