11cbce4fc1fc16cdee4f5b97e8e69d7c6eecee1a
Başar Demir <43862644+demirba18@users.noreply.github.com>
2018-12-26 22:35:17 +0300

    Add files via upload
    Comment bar is running but need improvements about its style

--- bottle_app.py
+++ bottle_app.py
@@ -5,29 +5,73 @@
 ### The landing page for assignment 3 should be at /
 #####################################################################
 
-from bottle import route, run, default_app, debug
+from bottle import route, run, default_app, debug, static_file, template, Bottle, request, SimpleTemplate
+from hashlib import sha256
 
-def htmlify(title,text):
-    page = """
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8" />
-                <title>%s</title>
-            </head>
-            <body>
-            %s
-            </body>
-        </html>
 
-    """ % (title,text)
-    return page
+
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
 
 def index():
-    return htmlify("My lovely website",
-                   "This is going to be an awesome website, when it is finished.")
-
+	return template("index")
 route('/', 'GET', index)
+
+def index_html():
+	return template("index")
+route('/index.html', 'GET', index_html)
+
+def about():
+	return template("about")
+route('/about.html', 'GET', about)
+
+def contacts():
+	return template("contacts")
+route('/contacts.html', 'GET', contacts)
+
+def elon():
+	return template("elonmusk")
+route('/elonmusk.html', 'GET', elon)
+
+def fail():
+	return template("failures")
+route('/failures.html', 'GET', fail)
+
+def launch():
+	return template("launches")
+route('/launches.html', 'GET', launch)
+
+def static_file_callback(filename):
+	return static_file(filename, root='./')
+route('/static/<filename>', 'GET', static_file_callback)
+
+def photos(filename):
+	return static_file(filename, root="./photos")
+route('/static/photos/<filename>', 'GET', photos)	
+
+def css(filename):
+	return static_file(filename, root="./css")
+route('/static/css/<filename>', 'GET', css)	
+
+def comment(filename):
+    comments = request.forms.comment.strip()
+    pswrd = request.forms.password
+    hsh1 = create_hash(pswrd)
+    point= " ("  + " rate: " + request.forms.get("point") + " )" + "\n"
+    while True:
+        if hsh1 == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92":
+            comment = open("comment.txt", "a+", encoding='utf-8')
+            comment.write(comments + point)
+            comment.close()
+            return template('./contacts.html')
+        else:
+            return template("./wrong.html")       
+route('/<filename>', 'POST', comment)
+
+
+
+	
 
 #####################################################################
 ### Don't alter the below code.
@@ -42,4 +86,3 @@ app = default_app()
 # The below code is necessary for running this bottle app standalone on your computer.
 if __name__ == "__main__":
   run()
-
